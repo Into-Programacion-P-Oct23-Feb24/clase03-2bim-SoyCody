5035aff9bdd8438f9468b4ed1c3c72ca3be44c1e
Cody <codycabreraa@gmail.com>
2023-12-20 15:34:50 -0500

    Paquete

---
+++ Ejemplosc3/src/Paquete05/Ejemplo01.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Paquete05;
+
+/**
+ *
+ * @author LAB.ELECT
+ */
+public class Ejemplo01 {
+    public static void main(String[] args) {
+        int[] arreglo1 = new int[3];
+        for (int n = 0; n < arreglo1.length; n++){
+            System.out.println(arreglo1[n]);
+        }        
+    }
+}
